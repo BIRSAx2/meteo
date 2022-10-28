a6803e30876d2c043a6b7dbe76660ee3c0ecde05
BIRSAx2 <mouhieddinesabir@gmail.com>
2022-10-28 15:23:04 +0200

    UI improvements

---
+++ src/components/Droplet.vue
@@ -0,0 +1,32 @@
+<template>
+  <i class="droplet fa-sharp fa-solid fa-droplet"></i>
+</template>
+
+<script setup>
+const props = defineProps({
+  fillPercent: {
+    type: String,
+    default: () => "0%",
+  },
+});
+</script>
+
+<style scoped>
+.droplet {
+  display: inline-block;
+  position: relative;
+  font-size: 20;
+  color: #ddd;
+}
+.droplet:after {
+  font-family: FontAwesome;
+  content: "\f043";
+  position: absolute;
+  left: 0;
+  top: 0px;
+  width: 100%;
+  height: v-bind("props.fillPercent");
+  overflow: hidden;
+  color: #60a5fa;
+}
+</style>
